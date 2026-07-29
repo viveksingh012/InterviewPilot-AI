d0bdde36ee6be022feab6afdd90df503b2cc2ecd
Vivek Singh <your-email@example.com>
2026-07-29 16:52:00 +0530

    feat: authetication login register refresh token access token and logout

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,23 @@
 import express from "express";
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 import routes from "./routes/index.js"
 import notFound from "./middleware/notFound.middleware..js";
 import prisma from "./config/prisma.js";
+import authMiddleware from "./middleware/authMiddleware.js";
 
 dotenv.config();
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended : true}))
 app.use(express.static("public"))
+app.use(cookieParser())
 
 
 app.use("/api/v1",routes)
 
 
-app.get("/",(req, res)=>{
+app.get("/",authMiddleware,(req, res)=>{
     res.send("working state")
 });
 
